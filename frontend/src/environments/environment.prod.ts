a47169c11728046ec050d42865f2fe6e84ed3679
Nolan Seokane <nolan@thedigitalacademy.co.za>
2021-04-21 14:49:13 +0200

    Containarizing the project on Docker

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  URL: 'http://127.0.0.1:8000/api'
+  URL: 'http://127.0.0.1:8000/api/v1'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  URL: `http://127.0.0.1:8000/api`
+  URL: `http://127.0.0.1:8000/api/v1`
 };
 
 /*
